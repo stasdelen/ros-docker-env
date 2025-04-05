8b9b5ffcfc0cddd62dfea407d2c1d40ffafcdccc
stasdelen <salih.tasdelen@hotmail.com>
2025-04-05 21:57:28 +0300

    add local feature

---
+++ .devcontainer/local-feature/install.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+set -e
+
+# System packages
+sudo apt-get update
+sudo apt-get install -y software-properties-common
+sudo apt install -y fuse libfuse2 wget
+
+sudo add-apt-repository ppa:deadsnakes/ppa -y
+sudo apt-get install -y python3.8 python3.8-venv
+
+wget https://github.com/neovim/neovim-releases/releases/download/v0.11.0/nvim-linux-x86_64.appimage
+chmod a+x nvim-linux-x86_64.appimage 
+sudo mv nvim-linux-x86_64.appimage /usr/bin/nvim
+
+git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
+~/.fzf/install --key-bindings --completion --update-rc
