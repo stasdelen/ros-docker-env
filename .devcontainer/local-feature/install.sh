0ec82ea211ae266aed84f916a996f523a9b722e6
SalihTasdelen <salih.tasdelen@hotmail.com>
2025-04-09 14:24:19 +0300

    add unzip for mason

--- .devcontainer/local-feature/install.sh
+++ .devcontainer/local-feature/install.sh
@@ -4,7 +4,7 @@ set -e
 # System packages
 sudo apt-get update
 sudo apt-get install -y software-properties-common
-sudo apt install -y fuse libfuse2 wget
+sudo apt install -y fuse libfuse2 wget unzip
 
 sudo add-apt-repository ppa:deadsnakes/ppa -y
 sudo apt-get install -y python3.8 python3.8-venv
