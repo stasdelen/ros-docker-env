a3cf64d5078911db39bfa418da2b12bc2953b5fc
stasdelen <salih.tasdelen@hotmail.com>
2025-05-13 21:08:11 +0300

    remove neovim installations

--- .devcontainer/local-feature/install.sh
+++ .devcontainer/local-feature/install.sh
@@ -9,9 +9,5 @@ sudo apt install -y fuse libfuse2 wget
 sudo add-apt-repository ppa:deadsnakes/ppa -y
 sudo apt-get install -y python3.8 python3.8-venv
 
-wget https://github.com/neovim/neovim-releases/releases/download/v0.11.0/nvim-linux-x86_64.appimage
-chmod a+x nvim-linux-x86_64.appimage 
-sudo mv nvim-linux-x86_64.appimage /usr/bin/nvim
-
 su rosuser -c "git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf"
 su rosuser -c "~/.fzf/install --key-bindings --completion --update-rc"
